994f21f626a8062e026f4fa5fc4e8341e1af8299
Samuel Lee <sme.lee@genesislab.ai>
2022-07-14 18:10:29 +0900

    Ready for running
    - 실행 가능한 형태의 main.py 파일을 구현하였습니다.
    - requirements.txt 파일을 추가하였습니다.

---
+++ main.py
@@ -0,0 +1,34 @@
+from redis import StrictRedis
+
+from config import MainConfig
+from downloader import AwsS3Downloader
+from transcribers import AwsTranscriber, ClovaNestTranscriber
+from transcribers.enums import Vendor
+
+from runner import ParallelExecutor, TranscriptionRequestRunner
+
+import click
+import logging
+
+
+@click.command()
+@click.option('-c', '--config_path', default='config/config-local.yml', help='config yaml file path', show_default=True)
+def main(config_path: str):
+    config = MainConfig.load_from_yml(config_path)
+    config.logger.setup()
+    redis = StrictRedis(**config.redis.kwargs)
+    downloader = AwsS3Downloader(config.aws.bucket, **config.aws.session_param)
+    executor = ParallelExecutor(**config.executor.kwargs)
+
+    transcribers = {
+        Vendor.AWS: (True, AwsTranscriber(**config.aws.kwargs)),
+        Vendor.ClovaNest: (False, ClovaNestTranscriber(**{**config.clova_nest.kwargs, **config.aws.kwargs}))
+    }
+
+    logging.info('Starting Insight-ASR broker')
+    runner = TranscriptionRequestRunner(redis, transcribers, executor, downloader, config.cache.dirpath)
+    runner.poll()
+
+
+if __name__ == '__main__':
+    main()
