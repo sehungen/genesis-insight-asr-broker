586e41b61a28ec30130574dd78dca46fd8f7d503
Samuel Lee <sme.lee@genesislab.ai>
2022-07-14 12:16:05 +0000

    Support asynchronous result web hook
    - `ParallelExecutor`와 `TranscriptionRequestRunner` 두 기능에 비동기 결과 처리를 위한 웹훗 처리 기능을 지원합니다.

--- runner/executor.py
+++ runner/executor.py
@@ -1,5 +1,5 @@
 from concurrent.futures import ThreadPoolExecutor, Future
-from typing import Any, Callable
+from typing import Any, Callable, Union
 
 
 class ParallelExecutor:
@@ -7,6 +7,7 @@ class ParallelExecutor:
         self.__max_request = max_request
         self.__executor = ThreadPoolExecutor(max_workers=max_request)
 
-    def submit(self, request_fn: Callable, response_fn: Callable[[Future], Any]):
+    def submit(self, request_fn: Callable, response_fn: Union[Callable[[Future], Any], None]):
         future = self.__executor.submit(request_fn)
-        future.add_done_callback(response_fn)
+        if response_fn:
+            future.add_done_callback(response_fn)
--- runner/runner.py
+++ runner/runner.py
@@ -21,17 +21,28 @@ class TranscriptionRequestRunner:
         self.__downloader = downloader
         self.__tempdir = tempdir
 
-    def make_request_and_response(self, transcriber: BaseTranscriber, s3_file_key: str, data_key: str, lang: Lang, use_s3_directly: bool):
+    def make_request_and_response(self, transcriber: BaseTranscriber, s3_file_key: str, data_key: str, lang: Lang, use_webhook: bool):
         # 음성인식 요청
         def __request_fn():
             return data_key, transcriber.transcribe(s3_file_key, lang)
 
-        # 음성인식 응답
+        # 음성인식 응답 - 동기
         def __response_fn(_future: Future):
             data_key_from_request, transcription = _future.result()
             transcription_text = '' if transcription is None else transcription.tojson()
             self.__redis.set(data_key_from_request, transcription_text, ex=self.__live_timeout)
-        return __request_fn, __response_fn
+
+        # 음성인식 응답 - 비동기(웹훅)
+        def __webhook_response_fn(_future: Future):
+            data_key_from_request, token = _future.result()
+            if token:
+                # 토큰이 있다면 토큰 등록 후, 나중에 결과를 찾기 위한 토큰과 data_key 등록
+                self.__redis.set(f'transcriber-webhook:{token}', data_key_from_request, ex=self.__live_timeout)
+            else:
+                # 토큰 반환이 None 이라면 실패한것으로 간주
+                self.__redis.set(data_key_from_request, '', ex=self.__live_timeout)
+
+        return __request_fn, __webhook_response_fn if use_webhook else  __response_fn
 
     def poll(self):
         while True:
@@ -43,8 +54,8 @@ class TranscriptionRequestRunner:
             req = TranscriptionRequest.parse_obj(orjson.loads(req.decode()))
 
             if req.vendor in self.__transcribers:
-                use_s3_directly, transcriber = self.__transcribers[req.vendor]
-                request_fn, response_fn = self.make_request_and_response(transcriber, req.s3_file_key, req.data_key, req.lang, use_s3_directly)
+                use_webhook, transcriber = self.__transcribers[req.vendor]
+                request_fn, response_fn = self.make_request_and_response(transcriber, req.s3_file_key, req.data_key, req.lang, use_webhook)
                 self.__requester.submit(request_fn, response_fn)
             else:
                 # Transcriber 없음
